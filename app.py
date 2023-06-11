9f2a7ec2fbff3e1ac8c4f2eecdda565e87b61bb0
AdvJosh <26547329+AdvJosh@users.noreply.github.com>
2023-06-11 23:53:49 +0000

    Added headers and footers and fixed other things

--- app.py
+++ app.py
@@ -19,13 +19,51 @@ from flask import Flask, render_template, jsonify, request
 # Set up the Flask app
 app = Flask(__name__)
 
+login_status = True
 
 # Let's define the home route
 @app.route("/")
 def home():
   page_title = 'HSBR Forbes Training Home'
   return render_template('home.html',
-                        page_title = page_title)
+                        page_title = page_title,
+                        login_status = login_status)
+
+
+# Let's set up the login route
+@app.route("/login")
+def login():
+  page_title = 'Log in page'
+  return render_template('login.html',
+                        page_title = page_title,
+                        login_status = login_status)
+
+
+# Let's set up the all sessions route
+@app.route("/sessions")
+def allsessions():
+  page_title = 'All Sessions'
+  return render_template('allsessions.html',
+                        page_title = page_title,
+                        login_status = login_status)
+
+
+# Let's set up the your sessions page
+@app.route("/mysessions")
+def mysessions():
+  page_title = 'My Sessions'
+  return render_template('mysessions.html',
+                        page_title = page_title,
+                        login_status = login_status)
+
+
+# Let's set up the session sign up page
+@app.route("/sign-in")
+def sessionsignin():
+  page_title = 'Session Sign In'
+  return render_template('sessionsignin.html',
+                        page_title = page_title,
+                        login_status = login_status)
 
 
 # Now, let's run the dev server on every available interface
